f67f602cbc51bb0ba824ea09bb188ab5206a14c9
david31 <david31st@gmail.com>
2023-06-26 15:00:54 +0200

    mise en place ,du controllerResolver et de largumentResolver

--- public/index.php
+++ public/index.php
@@ -4,6 +4,8 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\RequestContext;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Matcher\UrlMatcher;
+use Symfony\Component\HttpKernel\Controller\ArgumentResolver;
+use Symfony\Component\HttpKernel\Controller\ControllerResolver;
 use Symfony\Component\Routing\Exception\ResourceNotFoundException;
 
 require __DIR__ . '/../vendor/autoload.php';
@@ -21,12 +23,19 @@ $context->fromRequest($request);
 
 $urlMatcher = new UrlMatcher($routes, $context);
 
+$controllerResolver = new ControllerResolver();
+$argumentResolver = new ArgumentResolver();
+
 $pathInfo = $request->getPathInfo();
 
 try {
     $attributes = ($urlMatcher->match($pathInfo)); // contient la route et les parametre
     $request->attributes->add($attributes);
-    $response = call_user_func($attributes['_controller'], $request);
+
+    $controller = $controllerResolver->getController($request);
+    $arguments = $argumentResolver->getArguments($request, $controller);
+
+    $response = call_user_func_array($controller, $arguments);
 } catch (ResourceNotFoundException $e) {
     $response->setContent("La page demandée n'existe pas.");
     $response->setStatusCode(404);
